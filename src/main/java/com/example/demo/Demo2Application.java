707d95a533e630e8a9c96564fd83d37e94c3328f
opi6opi6 <opi6@hanmail.net>
2024-01-31 21:05:40 +0900

    init

---
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Demo2Application {
+
+    @GetMapping("/test")
+    String test() {
+        System.out.println("test");
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(Demo2Application.class, args);
+    }
+
+}
